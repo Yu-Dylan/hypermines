d9ca44da5230467411ac23e4fc933ca24bff1c90
Dylan Yu <64719370+Yu-Dylan@users.noreply.github.com>
2023-11-04 11:32:18 -0500

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,451 @@
+"""
+6.101 Lab 7:
+Six Double-Oh Mines
+"""
+#!/usr/bin/env python3
+
+import typing
+import doctest
+
+# NO ADDITIONAL IMPORTS ALLOWED!
+
+
+def dump(game):
+    """
+    Prints a human-readable version of a game (provided as a dictionary)
+    """
+    keys = ("board", "dimensions", "state", "visible")
+    # ^ Uses only default game keys. If you modify this you will need
+    # to update the docstrings in other functions!
+    for key in keys:
+        val = game[key]
+        if isinstance(val, list) and val and isinstance(val[0], list):
+            print(f"{key}:")
+            for inner in val:
+                print(f"    {inner}")
+        else:
+            print(f"{key}:", val)
+
+
+# 2-D IMPLEMENTATION
+
+
+def new_game_2d(nrows, ncolumns, mines):
+    """
+    Start a new game.
+
+    Return a game state dictionary, with the 'dimensions', 'state', 'board' and
+    'visible' fields adequately initialized.
+
+    Parameters:
+       nrows (int): Number of rows
+       ncolumns (int): Number of columns
+       mines (list): List of mines, given in (row, column) pairs, which are
+                     tuples
+
+    Returns:
+       A game state dictionary
+
+    >>> dump(new_game_2d(2, 4, [(0, 0), (1, 0), (1, 1)]))
+    board:
+        ['.', 3, 1, 0]
+        ['.', '.', 1, 0]
+    dimensions: (2, 4)
+    state: ongoing
+    visible:
+        [False, False, False, False]
+        [False, False, False, False]
+    """
+    return new_game_nd((nrows, ncolumns), mines)
+
+
+def dig_2d(game, row, col):
+    """
+    Reveal the cell at (row, col), and, in some cases, recursively reveal its
+    neighboring squares.
+
+    Update game['visible'] to reveal (row, col).  Then, if (row, col) has no
+    adjacent mines (including diagonally), then recursively reveal (dig up) its
+    eight neighbors.  Return an integer indicating how many new squares were
+    revealed in total, including neighbors, and neighbors of neighbors, and so
+    on.
+
+    The state of the game should be changed to 'defeat' when at least one mine
+    is visible on the board after digging (i.e. game['visible'][mine_location]
+    == True), 'victory' when all safe squares (squares that do not contain a
+    mine) and no mines are visible, and 'ongoing' otherwise.
+
+    Parameters:
+       game (dict): Game state
+       row (int): Where to start digging (row)
+       col (int): Where to start digging (col)
+
+    Returns:
+       int: the number of new squares revealed
+
+    >>> game = {'dimensions': (2, 4),
+    ...         'board': [['.', 3, 1, 0],
+    ...                   ['.', '.', 1, 0]],
+    ...         'visible': [[False, True, False, False],
+    ...                  [False, False, False, False]],
+    ...         'state': 'ongoing'}
+    >>> dig_2d(game, 0, 3)
+    4
+    >>> dump(game)
+    board:
+        ['.', 3, 1, 0]
+        ['.', '.', 1, 0]
+    dimensions: (2, 4)
+    state: victory
+    visible:
+        [False, True, True, True]
+        [False, False, True, True]
+
+    >>> game = {'dimensions': [2, 4],
+    ...         'board': [['.', 3, 1, 0],
+    ...                   ['.', '.', 1, 0]],
+    ...         'visible': [[False, True, False, False],
+    ...                  [False, False, False, False]],
+    ...         'state': 'ongoing'}
+    >>> dig_2d(game, 0, 0)
+    1
+    >>> dump(game)
+    board:
+        ['.', 3, 1, 0]
+        ['.', '.', 1, 0]
+    dimensions: [2, 4]
+    state: defeat
+    visible:
+        [True, True, False, False]
+        [False, False, False, False]
+    """
+    return dig_nd(game, (row, col))
+
+
+def render_2d_locations(game, all_visible=False):
+    """
+    Prepare a game for display.
+
+    Returns a two-dimensional array (list of lists) of '_' (hidden squares),
+    '.' (mines), ' ' (empty squares), or '1', '2', etc. (squares neighboring
+    mines).  game['visible'] indicates which squares should be visible.  If
+    all_visible is True (the default is False), game['visible'] is ignored
+    and all cells are shown.
+
+    Parameters:
+       game (dict): Game state
+       all_visible (bool): Whether to reveal all tiles or just the ones allowed
+                    by game['visible']
+
+    Returns:
+       A 2D array (list of lists)
+
+    >>> game = {'dimensions': (2, 4),
+    ...         'state': 'ongoing',
+    ...         'board': [['.', 3, 1, 0],
+    ...                   ['.', '.', 1, 0]],
+    ...         'visible':  [[False, True, True, False],
+    ...                   [False, False, True, False]]}
+    >>> render_2d_locations(game, False)
+    [['_', '3', '1', '_'], ['_', '_', '1', '_']]
+
+    >>> render_2d_locations(game, True)
+    [['.', '3', '1', ' '], ['.', '.', '1', ' ']]
+    """
+    return render_nd(game, all_visible)
+
+
+def render_2d_board(game, all_visible=False):
+    """
+    Render a game as ASCII art.
+
+    Returns a string-based representation of argument 'game'.  Each tile of the
+    game board should be rendered as in the function
+        render_2d_locations(game)
+
+    Parameters:
+       game (dict): Game state
+       all_visible (bool): Whether to reveal all tiles or just the ones allowed
+                           by game['visible']
+
+    Returns:
+       A string-based representation of game
+
+    >>> render_2d_board({'dimensions': (2, 4),
+    ...                  'state': 'ongoing',
+    ...                  'board': [['.', 3, 1, 0],
+    ...                            ['.', '.', 1, 0]],
+    ...                  'visible':  [[True, True, True, False],
+    ...                            [False, False, True, False]]})
+    '.31_\\n__1_'
+    """
+    out = ""
+    for row in render_2d_locations(game, all_visible):
+        for col in row:
+            out += col
+        out += "\n"
+    return out[:-1]
+
+
+# N-D IMPLEMENTATION
+
+
+def make_board(dimensions, value):
+    """
+    Make an N-D board with all values = value.
+    """
+    if len(dimensions) == 1:
+        return [value] * dimensions[0]
+    else:
+        return [make_board(dimensions[1:], value) for i in range(dimensions[0])]
+
+
+def get_cell(board, pos):
+    """
+    Get the value of a cell at pos in board.
+    """
+    if len(pos) == 1:
+        return board[pos[0]]
+    else:
+        return get_cell(board[pos[0]], pos[1:])
+
+
+def set_cell(board, pos, value):
+    """
+    Set a cell at pos in board to val.
+    """
+    if len(pos) == 1:
+        board[pos[0]] = value
+    else:
+        set_cell(board[pos[0]], pos[1:], value)
+
+
+def get_neighbors(dimensions, pos):
+    """
+    Get the neighbors of a cell at pos.
+    """
+    if len(pos) == 0:
+        yield ()
+    else:
+        for i in get_neighbors(dimensions[1:], pos[1:]):
+            for j in (pos[0] - 1, pos[0], pos[0] + 1):
+                if j >= 0 and j < dimensions[0]:
+                    yield (j,) + i
+
+
+def all_coords(dimensions):
+    """
+    Get all coordinates in a board.
+    """
+    if len(dimensions) == 0:
+        yield ()
+    else:
+        for i in range(dimensions[0]):
+            for j in all_coords(dimensions[1:]):
+                yield (i,) + j
+
+
+def is_win(game):
+    """
+    If game is won, return True. Otherwise, return False.
+    """
+    for pos in all_coords(game["dimensions"]):
+        if get_cell(game["board"], pos) != "." and not get_cell(game["visible"], pos):
+            return False
+    return True
+
+
+def new_game_nd(dimensions, mines):
+    """
+    Start a new game.
+
+    Return a game state dictionary, with the 'dimensions', 'state', 'board' and
+    'visible' fields adequately initialized.
+
+    Args:
+       dimensions (tuple): Dimensions of the board
+       mines (list): mine locations as a list of tuples, each an
+                     N-dimensional coordinate
+
+    Returns:
+       A game state dictionary
+
+    >>> g = new_game_nd((2, 4, 2), [(0, 0, 1), (1, 0, 0), (1, 1, 1)])
+    >>> dump(g)
+    board:
+        [[3, '.'], [3, 3], [1, 1], [0, 0]]
+        [['.', 3], [3, '.'], [1, 1], [0, 0]]
+    dimensions: (2, 4, 2)
+    state: ongoing
+    visible:
+        [[False, False], [False, False], [False, False], [False, False]]
+        [[False, False], [False, False], [False, False], [False, False]]
+    """
+    board = make_board(dimensions, 0)
+    for mine in mines:
+        set_cell(board, mine, ".")
+        for neighbor in get_neighbors(dimensions, mine):
+            neighbor_value = get_cell(board, neighbor)
+            if neighbor_value != ".":
+                set_cell(board, neighbor, neighbor_value + 1)
+    return {
+        "dimensions": dimensions,
+        "board": board,
+        "visible": make_board(dimensions, False),
+        "state": "ongoing",
+    }
+
+
+def dig_nd(game, coordinates):
+    """
+    Recursively dig up square at coords and neighboring squares.
+
+    Update the visible to reveal square at coords; then recursively reveal its
+    neighbors, as long as coords does not contain and is not adjacent to a
+    mine.  Return a number indicating how many squares were revealed.  No
+    action should be taken and 0 returned if the incoming state of the game
+    is not 'ongoing'.
+
+    The updated state is 'defeat' when at least one mine is visible on the
+    board after digging, 'victory' when all safe squares (squares that do
+    not contain a mine) and no mines are visible, and 'ongoing' otherwise.
+
+    Args:
+       coordinates (tuple): Where to start digging
+
+    Returns:
+       int: number of squares revealed
+
+    >>> g = {'dimensions': (2, 4, 2),
+    ...      'board': [[[3, '.'], [3, 3], [1, 1], [0, 0]],
+    ...                [['.', 3], [3, '.'], [1, 1], [0, 0]]],
+    ...      'visible': [[[False, False], [False, True], [False, False],
+    ...                [False, False]],
+    ...               [[False, False], [False, False], [False, False],
+    ...                [False, False]]],
+    ...      'state': 'ongoing'}
+    >>> dig_nd(g, (0, 3, 0))
+    8
+    >>> dump(g)
+    board:
+        [[3, '.'], [3, 3], [1, 1], [0, 0]]
+        [['.', 3], [3, '.'], [1, 1], [0, 0]]
+    dimensions: (2, 4, 2)
+    state: ongoing
+    visible:
+        [[False, False], [False, True], [True, True], [True, True]]
+        [[False, False], [False, False], [True, True], [True, True]]
+    >>> g = {'dimensions': (2, 4, 2),
+    ...      'board': [[[3, '.'], [3, 3], [1, 1], [0, 0]],
+    ...                [['.', 3], [3, '.'], [1, 1], [0, 0]]],
+    ...      'visible': [[[False, False], [False, True], [False, False],
+    ...                [False, False]],
+    ...               [[False, False], [False, False], [False, False],
+    ...                [False, False]]],
+    ...      'state': 'ongoing'}
+    >>> dig_nd(g, (0, 0, 1))
+    1
+    >>> dump(g)
+    board:
+        [[3, '.'], [3, 3], [1, 1], [0, 0]]
+        [['.', 3], [3, '.'], [1, 1], [0, 0]]
+    dimensions: (2, 4, 2)
+    state: defeat
+    visible:
+        [[False, True], [False, True], [False, False], [False, False]]
+        [[False, False], [False, False], [False, False], [False, False]]
+    """
+
+    def num_of_squares(game, pos):
+        """
+        Helper to reveal squares.
+        """
+        count = 1
+        if get_cell(game["board"], pos) != 0:
+            return count
+        for neighbor in get_neighbors(game["dimensions"], pos):
+            if not get_cell(game["visible"], neighbor):
+                set_cell(game["visible"], neighbor, True)
+                count += num_of_squares(game, neighbor)
+        return count
+
+    if game["state"] != "ongoing" or get_cell(game["visible"], coordinates):
+        return 0
+    set_cell(game["visible"], coordinates, True)
+    if get_cell(game["board"], coordinates) == ".":
+        game["state"] = "defeat"
+        return 1
+    squares = num_of_squares(game, coordinates)
+    if is_win(game):
+        game["state"] = "victory"
+    return squares
+
+
+def render_nd(game, all_visible=False):
+    """
+    Prepare the game for display.
+
+    Returns an N-dimensional array (nested lists) of '_' (hidden squares), '.'
+    (mines), ' ' (empty squares), or '1', '2', etc. (squares neighboring
+    mines).  The game['visible'] array indicates which squares should be
+    visible.  If all_visible is True (the default is False), the game['visible']
+    array is ignored and all cells are shown.
+
+    Args:
+       all_visible (bool): Whether to reveal all tiles or just the ones allowed
+                           by game['visible']
+
+    Returns:
+       An n-dimensional array of strings (nested lists)
+
+    >>> g = {'dimensions': (2, 4, 2),
+    ...      'board': [[[3, '.'], [3, 3], [1, 1], [0, 0]],
+    ...                [['.', 3], [3, '.'], [1, 1], [0, 0]]],
+    ...      'visible': [[[False, False], [False, True], [True, True],
+    ...                [True, True]],
+    ...               [[False, False], [False, False], [True, True],
+    ...                [True, True]]],
+    ...      'state': 'ongoing'}
+    >>> render_nd(g, False)
+    [[['_', '_'], ['_', '3'], ['1', '1'], [' ', ' ']],
+     [['_', '_'], ['_', '_'], ['1', '1'], [' ', ' ']]]
+
+    >>> render_nd(g, True)
+    [[['3', '.'], ['3', '3'], ['1', '1'], [' ', ' ']],
+     [['.', '3'], ['3', '.'], ['1', '1'], [' ', ' ']]]
+    """
+    out = make_board(game["dimensions"], 0)
+    for pos in all_coords(game["dimensions"]):
+        val = get_cell(game["board"], pos)
+        vis = get_cell(game["visible"], pos)
+        if not all_visible and not vis:
+            set_cell(out, pos, "_")
+        elif val == 0:
+            set_cell(out, pos, " ")
+        else:
+            set_cell(out, pos, str(val))
+    return out
+
+
+if __name__ == "__main__":
+    # Test with doctests. Helpful to debug individual lab.py functions.
+    _doctest_flags = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
+    doctest.testmod(optionflags=_doctest_flags)  # runs ALL doctests
+
+    # game = new_game_nd((10, 20, 3), [(1, 2, 0)])
+    # for coord in get_neighbors(game["dimensions"], (5, 13, 0)):
+    #     print(str(coord) + ",")
+
+    # Alternatively, can run the doctests JUST for specified function/methods,
+    # e.g., for render_2d_locations or any other function you might want.  To
+    # do so, comment out the above line, and uncomment the below line of code.
+    # This may be useful as you write/debug individual doctests or functions.
+    # Also, the verbose flag can be set to True to see all test results,
+    # including those that pass.
+    #
+    # doctest.run_docstring_examples(
+    #    render_2d_locations,
+    #    globals(),
+    #    optionflags=_doctest_flags,
+    #    verbose=False
+    # )
